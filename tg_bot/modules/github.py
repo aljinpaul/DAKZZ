af91a05eb2946e45b24828e35df2cc39de6e5747
ABBAS MHD SALIM <60001289+ABBASMHDSALIMZDX@users.noreply.github.com>
2020-01-24 06:27:11 +0000

    Add files via upload

---
+++ tg_bot/modules/github.py
@@ -0,0 +1,48 @@
+from telegram import ParseMode, Update, Bot
+from telegram.ext import run_async
+
+from tg_bot.modules.disable import DisableAbleCommandHandler
+from tg_bot import dispatcher
+
+from requests import get
+
+
+@run_async
+def github(bot: Bot, update: Update):
+    message = update.effective_message
+    text = message.text[len('/git '):]
+    usr = get(f'https://api.github.com/users/{text}').json()
+    if usr.get('login'):
+        reply_text = f"""*Name:* `{usr['name']}`
+👨‍💼*Username:* `{usr['login']}`
+🔖*Account ID:* `{usr['id']}`
+📝*Account type:* `{usr['type']}`
+📍*Location:* `{usr['location']}`
+✍️*Bio:* `{usr['bio']}`
+➡️*Followers:* `{usr['followers']}`
+⬅️*Following:* `{usr['following']}`
+🌚*Hireable:* `{usr['hireable']}`
+⛵️*Public Repos:* `{usr['public_repos']}`
+🚁*Public Gists:* `{usr['public_gists']}`
+📨*Email:* `{usr['email']}`
+🏢*Company:* `{usr['company']}`
+🌍*Website:* `{usr['blog']}`
+♻️*Last updated:* `{usr['updated_at']}`
+📒*Account created at:* `{usr['created_at']}`
+"""
+    else:
+        reply_text = "User not found. Make sure you entered valid username!"
+    message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN)
+
+
+__help__ = """
+         
+    - /git:{GitHub username} :ഗിറ്റ്ഹബ്  ഉപയോക്താവിന്റെ വിവരങ്ങൾ നൽകുന്നു.
+           
+"""
+
+__mod_name__ = "Github User Information"
+
+github_handle = DisableAbleCommandHandler("git", github)
+
+dispatcher.add_handler(github_handle)
