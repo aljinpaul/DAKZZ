dbeadcbc7dee6626fd21e9925479b7ca34b2e4d1
ABBAS MHD SALIM <60001289+ABBASMHDSALIMZDX@users.noreply.github.com>
2020-01-30 06:54:17 +0000

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,7 +18,7 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-Hey there! My name is PsYcHo DakzZ
+Hey there! My name is Mr Mysterious 
 I'm here to help you manage your groups!
 
  - /help: help....
